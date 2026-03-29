2eb1104691b0a61d53036226c997f1636e426d80
Arher695 <sorokinivan695@gmail.com>
2026-03-29 16:14:00 +0700

    Add files via upload

---
+++ DockerDemo/src/main/java/com/example/DockerDemo/DockerDemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.DockerDemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DockerDemoApplication {
+    @Value("${inst.num:1}")
+    private int num;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World! " + num + " ver.2";
+    }
+
+}
